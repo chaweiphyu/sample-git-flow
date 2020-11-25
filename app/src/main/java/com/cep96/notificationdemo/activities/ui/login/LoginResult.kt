8536ec99beb6a642ab88a0c0bd8e96eaa0acef11
chaw96 <chaweiphyu@securelinkmm.com>
2020-11-25 13:40:22 +0630

    setup login

---
+++ app/src/main/java/com/cep96/notificationdemo/activities/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.cep96.notificationdemo.activities.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
